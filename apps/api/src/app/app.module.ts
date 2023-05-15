e58f5d156084569c38825928fcff2465d294f7c0
bimendra <bimendra.85@gmail.com>
2023-05-15 11:33:06 +0800

    Import modules prisma, user and notes into app module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { UsersModule } from '../users/users.module';
+import { NotesModule } from '../notes/notes.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UsersModule, NotesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService) {}
+  async create(createUserDto: CreateUserDto) {
+    return await this.prisma.user.create({
+      data: createUserDto,
+    });
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return await this.prisma.user.findMany();
   }
 
   findOne(id: number) {
